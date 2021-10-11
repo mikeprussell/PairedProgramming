98b73a0859549835d82cbddf74c6ae57107d371f
mikeprussell <mikeprussell@outlook.com>
2021-10-11 13:34:51 -0500

    Initial commit

---
+++ PairedProgramming/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairedProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string continueStudent;
+            ArrayList students = new ArrayList();
+
+            do
+            {
+                Console.WriteLine("Enter student name:");
+                students.Add(Console.ReadLine());
+
+                Console.WriteLine("Would you like to enter another student name? (Y/N)");
+                continueStudent = Console.ReadLine().ToUpper();
+
+            } while (continueStudent != "N");
+
+            int studentCount = students.Count;
+
+            if (studentCount % 2 == 0)
+            {
+                Console.WriteLine(student1);
+            }
+
+
+        }
+    }
+}
